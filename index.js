55d4907c49f20db11b8f5b96e3d58d181b1587c4
esterlilianlb <esterlilian2231@gmail.com>
2020-06-12 11:33:02 -0300

    separando classes em arquivos diferentes

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-class Cliente {
-  nome;
-  cpf;
-}
-
-class ContaCorrente {
-  agencia;
-  saldo;
-
-  sacar(valor) {
-    if(this.saldo >= valor) {
-      this.saldo -=valor;
-    }
-  };
-}
-
-
 const cliente1 = new Cliente();
 
 cliente1.nome = 'Ricardo';
@@ -28,16 +11,13 @@ cliente2.cpf = 88822233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
 
-contaCorrenteRicardo.saldo = 0;
 contaCorrenteRicardo.agencia = 1001;
-console.log(contaCorrenteRicardo.saldo);
 
-contaCorrenteRicardo.saldo = 100;
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.sacar(50);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
 
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado)
 
-console.log(contaCorrenteRicardo.saldo)
-
-console.log(cliente1)
-console.log(cliente2)
+console.log(contaCorrenteRicardo);
